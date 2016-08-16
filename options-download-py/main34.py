0c3ad8a400448a9e0e455e054ba0a93ab0d5b4f4
Connor Popik <popik@mit.edu>
2016-08-16 16:43:49 -0400

    updated for multi-core processing

--- options-download-py/main34.py
+++ options-download-py/main34.py
@@ -4,38 +4,36 @@
 """
 
 from nasdaqOptions import *
-from threading import Thread
-from queue import Queue
+from multiprocessing import Process, JoinableQueue, cpu_count
 import json
+import time
 
 # num worker queues
-numWorkerQueues = 10
+numWorkerQueues = cpu_count()
 
 def worker():
     # task codes
     # 0: get last page and add URL to queue
     # 1: get dataset from queue and ?upload?
 
-    while True:
+    while not q.empty():
         task = q.get()
 
-        # break when there are no more tasks
-        if task is None:
-            break
-        elif task[0] == 0:
+        if task[0] == 0:
             ticker = task[1]
             ticker, lastPage = getLastPage(ticker)  # should remove ticker in future versions
             # adds to global list all the necessary pages
             for page in range(1, lastPage+1):
                 # add a task with type 1 to the queue
-                url = 'http://www.nasdaq.com/symbol/' + ticker + '/option-chain?money=all&dateindex=-1&page='+str(page)
+                url = 'http://www.nasdaq.com/symbol/' + ticker + '/option-chain?&dateindex=-1&page='+str(page)
                 q.put([1, ticker, url])
-                print('Added: ', url)
+                print('Added: ', url, '\t\t\t', time.strftime('%H:%M:%S'))
+
         elif task[0] == 1:
             ticker = task[1]
             url = task[2]
             downloadOptionsPage(ticker, url)
-            print('Uploaded: ', url)
+            print('Uploaded: ', url, '\t\t\t', time.strftime('%H:%M:%S'))
 
         # tell the queue that the task is done
         q.task_done()
@@ -45,26 +43,35 @@ def main():
     tickers = json.loads(openFile.read())
     openFile.close()
 
+    # create global queue
     global q
-    q = Queue()
+    q = JoinableQueue()
 
-    threads = []
+    # starting up
+    print("Initializing on ", cpu_count(), ' cores')
+
+    # create multiple processing threads
+    activeProcesses = []
     for i in range(numWorkerQueues):
-        thread = threading.Thread(target = worker)
+        process = Process(target = worker)
         # thread.daemon = True
-        thread.start()
-        threads.append(thread)
+        process.start()
+        activeProcesses.append(process)
+
+    # starting up
+    print("Adding processes...")
 
     for ticker in tickers:
         q.put([0, ticker])
 
     # block until all tasks are done
     q.join()
-    # stop workers
-    for i in range(numWorkerQueues):
-        q.put(None)
-    for t in threads:
-        thread.join()
+
+    # # stop workers
+    # for i in range(numWorkerQueues):
+    #     q.put(None)
+    # for p in activeProcesses:
+    #     p.join()
 
 if __name__ == "__main__":
     main()
