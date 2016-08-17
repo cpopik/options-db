f03685d55dd988556caad6ef09b79f23417814eb
Connor Popik <popik@mit.edu>
2016-08-17 18:03:33 -0400

    config file update

---
+++ options-download-py/cronRun.sh
@@ -0,0 +1,9 @@
+#!/bin/tcsh
+
+setenv USER cpopik
+setenv LOGNAME cpopik
+setenv HOME /home/cpopik
+setenv PATH ${PATH}:/bin:/usr/bin:/sbin:/usr/local/bin:/usr/local/MATLAB/R2015b/bin
+setenv LANG en_US.UTF-8
+
+ python3 /home/cpopik/options-db/options-download-py/main.py
